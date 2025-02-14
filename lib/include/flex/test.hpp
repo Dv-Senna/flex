33278853c2838e17c9c6bad77822fcf88be2e1fd
Dv-Senna <dvsenna64@gmail.com>
2025-02-14 01:55:39 +0100

    Change cap of flex

--- lib/include/flex/test.hpp
+++ lib/include/flex/test.hpp
@@ -1,29 +1,8 @@
 #pragma once
 
-#include <print>
-
-#include "flex/config.hpp"
-
 
 namespace flex {
 	inline void sayHello() {
-		if constexpr (flex::config::OS == flex::OS::eWindows)
-			std::println("Hello from Windows flex !");
-		else if constexpr (flex::config::OS == flex::OS::eLinux)
-			std::println("Hello from Linux flex !");
-		else if constexpr (flex::config::OS == flex::OS::eMacos)
-			std::println("Hello from Apple flex !");
 
-		if constexpr (flex::config::COMPILER == flex::Compiler::eClang)
-			std::println("Using clang as a compiler");
-		else if constexpr (flex::config::COMPILER == flex::Compiler::eGCC)
-			std::println("Using gcc as a compiler");
-		else if constexpr (flex::config::COMPILER == flex::Compiler::eMSVC)
-			std::println("Using MSVC as a compiler");
-
-		if constexpr (flex::config::BUILD_TYPE == flex::BuildType::eDebug)
-			std::println("Debug build");
-		else if constexpr (flex::config::BUILD_TYPE == flex::BuildType::eRelease)
-			std::println("Release build");
 	}
 }
