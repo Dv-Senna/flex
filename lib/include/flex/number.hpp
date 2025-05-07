3d934e3bc5b906a7a07263c2a8c2fa634f9026fc
Dv-Senna <dvsenna64@gmail.com>
2025-05-07 18:02:53 +0200

    Add forgotten operator and constexpr

--- lib/include/flex/number.hpp
+++ lib/include/flex/number.hpp
@@ -18,20 +18,23 @@ namespace flex {
 			constexpr Number(Self&&) noexcept = default;
 			constexpr auto operator=(Self&&) noexcept -> Self& = default;
 
-			auto operator==(const Self&) const noexcept -> bool = default;
-			auto operator<=>(const Self&) const noexcept = default;
+			constexpr auto operator==(const Self&) const noexcept -> bool = default;
+			constexpr auto operator<=>(const Self&) const noexcept = default;
 
-			auto operator+=(const Self &self) noexcept -> Self& {m_value += self.m_value; return *this;}
-			auto operator-=(const Self &self) noexcept -> Self& {m_value -= self.m_value; return *this;}
-			auto operator*=(const Self &self) noexcept -> Self& {m_value *= self.m_value; return *this;}
-			auto operator/=(const Self &self) noexcept -> Self& {m_value /= self.m_value; return *this;}
+			constexpr auto operator+=(const Self &self) noexcept -> Self& {m_value += self.m_value; return *this;}
+			constexpr auto operator-=(const Self &self) noexcept -> Self& {m_value -= self.m_value; return *this;}
+			constexpr auto operator*=(const Self &self) noexcept -> Self& {m_value *= self.m_value; return *this;}
+			constexpr auto operator/=(const Self &self) noexcept -> Self& {m_value /= self.m_value; return *this;}
 
-			auto operator+(const Self &self) const noexcept -> Self {return {m_value + self.m_value};}
-			auto operator-(const Self &self) const noexcept -> Self {return {m_value - self.m_value};}
-			auto operator*(const Self &self) const noexcept -> Self {return {m_value * self.m_value};}
-			auto operator/(const Self &self) const noexcept -> Self {return {m_value / self.m_value};}
+			constexpr auto operator+(const Self &self) const noexcept -> Self {return {m_value + self.m_value};}
+			constexpr auto operator-(const Self &self) const noexcept -> Self {return {m_value - self.m_value};}
+			constexpr auto operator*(const Self &self) const noexcept -> Self {return {m_value * self.m_value};}
+			constexpr auto operator/(const Self &self) const noexcept -> Self {return {m_value / self.m_value};}
 
-			auto operator%(const Self &self) const noexcept -> Self requires (std::is_integral_v<T>) {return {m_value % self.m_value};}
+			constexpr auto operator%(const Self &self) const noexcept -> Self requires (std::is_integral_v<T>) {return {m_value % self.m_value};}
+
+			constexpr operator T() const noexcept {return m_value;}
+
 
 		private:
 			T m_value;
